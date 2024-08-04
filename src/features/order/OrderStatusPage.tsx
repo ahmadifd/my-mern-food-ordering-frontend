a3466553a207fa560ba95f1258428d2153e92de2
ahmadifd <ahmadi.fd@gmail.com>
2024-08-04 18:44:56 +0330

    23432

--- src/features/order/OrderStatusPage.tsx
+++ src/features/order/OrderStatusPage.tsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Card, CardContent } from "@mui/material";
 import { useGetMyOrdersQuery } from "./orderApiSlice";
 import OrderStatusHeader from "./OrderStatusHeader";
 import OrderStatusDetail from "./OrderStatusDetail";
@@ -14,28 +14,41 @@ const OrderStatusPage = () => {
 
   return (
     <>
-      {isLoading ? (
-       <h1>...isLoading</h1>
-      ) : data?.length ? (
-        data?.map((order: Order, index: number) => (
-          <Box
-            key={index}
-            mb={5}
-            sx={{
-              backgroundColor: "grey.100",
-              padding: "1em",
-              borderRadius: "0.5em",
-            }}
-          >
-            <OrderStatusHeader order={order} />
-            <OrderStatusDetail order={order} />
+      <Box
+        sx={{
+          backgroundColor: "grey.100",
+          padding: "1em",
+          borderRadius: "0.5em",
+          minHeight: "100%",
+        }}
+      >
+        {isLoading ? (
+          <h1>...isLoading</h1>
+        ) : data?.length ? (
+          data?.map((order: Order, index: number) => (
+            <Box
+              key={index}
+              mb={5}
+              sx={{
+                backgroundColor: "grey.100",
+                padding: "1em",
+                borderRadius: "0.5em",
+              }}
+            >
+              <Card>
+                <CardContent>
+                  <OrderStatusHeader order={order} />
+                  <OrderStatusDetail order={order} />
+                </CardContent>
+              </Card>
+            </Box>
+          ))
+        ) : (
+          <Box fontWeight="bold" fontSize="1.3rem">
+            No orders
           </Box>
-        ))
-      ) : (
-        <Box fontWeight="bold" fontSize="1.3rem">
-          No orders
-        </Box>
-      )}
+        )}
+      </Box>
     </>
   );
 };
